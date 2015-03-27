2e98ab964f09576fe180a6c80c941a7d24ab0de1
Kurt Krueckeberg <kurtk@pobox.com>
2015-03-26 21:28:38 -0500

    Added to csvreader.cpp

--- csvreader.cpp
+++ csvreader.cpp
@@ -16,55 +16,53 @@ CsvReader::CsvReader(string file_name, regex rgexp) : csv_regex(rgexp), line_no(
 
     if (input.is_open()) {
 
-    // TODO: Check for errors.
+       // TODO: Check for errors.
     }	    
-    csv_regex = rgexp;
 }
 
 CsvReader::getIterator()
 {    
-    if (input.good()) {
+  if (input.good()) {
+    
+     ++line_no;  
+
+     /*
+      * TODO:  Replace ::getline() with a class that gets the complete comments, reading ahead to check whether the signers comments continue onto the next line.            *
+      * and replacing double quotes in comments with single quotes. 
+      */
+     string line;
+
+     getline(input, line); 
+
+     regex two_dbl_quotes{"(\"\")"};
      
-          ++line_no;  
+     string adjusted_line = regex_replace(line, two_dbl_quotes, string{"'"});
+                        
+     try {
+           
+       smatch  match;
+       string  output;
 
-          /*
-           * TODO:  Replace ::getline() with a class that gets the complete comments, reading ahead to check whether the signers comments continue onto the next line.            *
-           * and replacing double quotes in comments with single quotes. 
-           */
-	  string line;
+       if (!line.empty()) {
+          // TODO: Change code from regex_search() to regex_iterator 
+          if (regex_search(adjusted_line, match, csv_regex) && match.size() > 1) {
+           
+             for(size_t i = 1; i < match.size(); ++i) {
+               
+               /*
+                * TODO: 1. Check whether signee_no exists in database. 
+                        2. If not, insert new record, converting dates and strings to MySQL dates and strings, respectively.
+                */ 
+              }
+           
+           
+           } else {
 
-          getline(input, line); 
+               // concatenate with prior line, continue loop                                    
 
-          regex two_dbl_quotes{"(\"\")"};
-          
-          string adjusted_line = regex_replace(line, two_dbl_quotes, string{"'"});
-                             
-          try {
-                
-            smatch  match;
-            string  output;
+           }
+       }
+     } catch (exception& e) {
 
-            if (!line.empty()) {
-
-
-               if (regex_search(adjusted_line, match, csv_regex) && match.size() > 1) {
-                
-                  for(size_t i = 1; i < match.size(); ++i) {
-                    
-                    /*
-                     * TODO: 1. Check whether signee_no exists in database. 
-                             2. If not, insert new record, converting dates and strings to MySQL dates and strings, respectively.
-                     */ 
-                   }
-                
-                
-                } else {
-
-                    // concatenate with prior line, continue loop                                    
-
-                }
-	    }
-	  } catch (exception& e) {
-
-          }
-    }	  
+     }
+}	  
