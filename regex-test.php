57af33b8e6d90369f5864361b016ccc1d76ba965
Kurt Krueckeberg <kurtk@pobox.com>
2015-03-30 21:44:23 -0500

    Added to csvreader.cpp

---
+++ regex-test.php
@@ -0,0 +1,13 @@
+<?php
+   
+// See Maude source code.
+ 
+  $file = new \SplFileObject();   
+  $file->
+
+  while ($file as $line_no => $text) {
+
+      $regex = '/^(\d+),(\d\d-\d\d-\d\d\d\d),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*)$/m'; 
+
+      $matches = preg_match(   ); 
+  }
