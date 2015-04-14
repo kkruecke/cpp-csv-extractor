c5cb8bd80d3b18e8c3b42e1b2bd858df2003de84
Kurt Krueckeberg <kurtk@pobox.com>
2015-04-14 10:36:32 -0500

    latest updates

--- csvparser.cpp
+++ csvparser.cpp
@@ -83,91 +83,85 @@ vector<string> CsvParser::parseNextLine()
         return strings;
  }    
 
- try {  
 
-   while (iter != end) {
-   
-     switch (comma_cnt) {
-   
-       case 0:
-
-          while( *iter++ !=',');
-
-         // cout << line.substr(0, iter - line.begin() - 1) << endl; debug code
-
-          strings.push_back(line.substr(0, iter - line.begin() - 1));
-   
-          break;  
-      
-       case 1: // date is a fixed length and can therefore be calculated 
-          
-        // cout << line.substr(iter - line.begin(), date_length) << endl; // debug only
-          
-          strings.push_back(line.substr(iter - line.begin(), date_length));
-   
-          iter += 10; // Does it point to comma now or the next string?
-          break;
-
-       default:
-              // All other cases are identical
-           // Question: Does good properly handle empty strings?
-       
-           auto start_offset = ++iter - line.begin(); // Initially iter is pointing at a comma. Advance it... 
-                      
-           if (iter == end || *iter == ',') { // Are we at the end of the string, or do we have an substring empty?
-               
-               strings.push_back(string{""}); // insert an empty string
-               break;
-               
-           }  else if (*++iter == '"') { // Check for enclosing quotes.
-   
-               while(*iter++ != '"'); // If string is enclosed in quotes, find terminating double quote...
-   
-               if (*iter++ != ',') { // ...and then check for the comma ...
-   
-               } else if (!*iter) { // ...or if end-of-string.
-
-                    throw domain_error("string is not a proper csv string");
-               }
-   
-          } else {
-
-               // If no enclosing double quotes, go to comma or end of string 
-               
-               for(;iter != end; ++iter) {
-                               
-                   if (*iter == ',') {
-                        break; // Bug
-                   }
-                }
-          } 
-          
-          int length = iter - line.begin() - start_offset; // debug only, combine below later
-           
-          auto temp_str = std::move( line.substr(start_offset, length) ); 
-           
-          // Strip enclosing quotes.
-          if (temp_str.front() == '"') {
-        
-               temp_str = temp_str.substr(1, temp_str.end() - temp_str.begin() - 2);
-          } 
-
-        // cout << temp_str << endl; debug code
+ while (iter != end) {
  
-          strings.push_back( temp_str );  
-       
-          break;
-      
-      } // end switch
+   switch (comma_cnt) {
+ 
+     case 0:
 
-      ++comma_cnt; 
-    } // end while   
+        while( *iter++ !=',');
 
- }  catch(exception& e) {
+       // cout << line.substr(0, iter - line.begin() - 1) << endl; debug code
+
+        strings.push_back(line.substr(0, iter - line.begin() - 1));
+ 
+        break;  
+    
+     case 1: // date is a fixed length and can therefore be calculated 
+        
+      // cout << line.substr(iter - line.begin(), date_length) << endl; // debug only
+        
+        strings.push_back(line.substr(iter - line.begin(), date_length));
+ 
+        iter += 10; // Does it point to comma now or the next string?
+        break;
+
+     default:
+            // All other cases are identical
+         // Question: Does good properly handle empty strings?
      
-        // catch-all for C++11 exceptions 
-        cerr << "Exception caught in parse(line): " << e.what() << '\n';
- }
+         auto start_offset = ++iter - line.begin(); // Initially iter is pointing at a comma. Advance it... 
+                    
+         if (iter == end || *iter == ',') { // Are we at the end of the string, or do we have an substring empty?
+             
+             strings.push_back(string{""}); // insert an empty string
+             break;
+             
+         }  else if (*++iter == '"') { // Check for enclosing quotes.
+ 
+             while(*iter++ != '"'); // If string is enclosed in quotes, find terminating double quote...
+ 
+             if (*iter++ != ',') { // ...and then check for the comma ...
+ 
+             } else if (!*iter) { // ...or if end-of-string.
+
+                  throw domain_error("string is not a proper csv string");
+             }
+ 
+        } else {
+
+             // If no enclosing double quotes, go to comma or end of string 
+             
+             for(;iter != end; ++iter) {
+                             
+                 if (*iter == ',') {
+                      break; // Bug
+                 }
+              }
+        } 
+        
+        int length = iter - line.begin() - start_offset; // debug only, combine below later
+         
+        auto temp_str = std::move( line.substr(start_offset, length) ); 
+         
+        // Strip enclosing quotes.
+        if (temp_str.front() == '"') {
+      
+             temp_str = temp_str.substr(1, temp_str.end() - temp_str.begin() - 2);
+        } 
+
+      // cout << temp_str << endl; debug code
+ 
+        strings.push_back( temp_str );  
+     
+        break;
+    
+    } // end switch
+
+    ++comma_cnt; 
+ } // end while   
+ 
    
  return strings;
    
--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
                                       
   CsvParser csv_parser(argv[1]); 
     
-   // TODO: A transaction support later.
+  // TODO: A transaction support later.
 
   // Credentials: (url, user, password)
   unique_ptr<Connection> conn { get_driver_instance()->connect("tcp://127.0.0.1:3306", "petition", "kk0457") };
@@ -52,123 +52,122 @@ int main(int argc, char** argv)
 
   unique_ptr<PreparedStatement> signer_comments_stmt { conn->prepareStatement("INSERT INTO signer_comments(signee_no, comments) VALUES(?, ?)") };
 
-while (csv_parser.hasmoreLines()) {  
+  while (csv_parser.hasmoreLines()) {  
 
-   vector<string> strings = csv_parser.parseNextLine();     
+  vector<string> strings = csv_parser.parseNextLine();     
 
-for(int i = 0; i < strings.size(); ++i) {
+  for(int i = 0; i < strings.size(); ++i) {
 
-   try {
-   
-        switch(i) {
-             
-             case 0:
-             {
-               int signee_no = stoi(strings[0]);
-               
-               signer_info_stmt->setInt(1, signee_no);
-               signer_comments_stmt->setInt(1, signee_no);
-             }
-             break;
-                 
-             case 1:    
-             { // DATE: YYY-MM-DD
-                 string date { strings[1].substr(6, 4) + "-" + strings[1].substr(0, 2) + "-" + strings[1].substr(3, 2) };
-                 
-                 signer_info_stmt->setDateTime(2, date);
-             }
-             break; 
-   
-             case 2:    
-             {   // First Name 
-                 
-                 if (strings[2].empty()) {
-                      // Per http://forums.mysql.com/read.php?167,419402,421088#msg-421088, the 2nd parameter cab be be 0.   
-	              signer_info_stmt->setNull(3, 0); 
-   
-                 } else {
-   
-                     signer_info_stmt->setString(3, strings[2]);
-                 }
-             }
-             break; 
-   
-             case 3:    
-             {  // Last Name 
-                 
-                 if (strings[3].empty()) {
-   
-                    signer_info_stmt->setNull(4, 0);
-                     
-                 } else {
-   
-                    signer_info_stmt->setString(4, strings[3]);
-                 }
-             }
-             break; 
-   
-             case 4:    
-             {   // City 
-                 
-                 if (strings[4].empty()) {
-   
-                    signer_info_stmt->setNull(5, 0);
-                     
-                 } else {
-   
-                    signer_info_stmt->setString(5, strings[4]);
-                 }
-             }
-             break; 
-              
-             case 5:    
+      try {
+      
+           switch(i) {
                 
-                 // Comments 
-                 if (strings[5].empty()) {
-   
-                    signer_comments_stmt->setNull(2, 0);
-                     
-                 } else {
-             
-                    signer_comments_stmt->setString(2, strings[5]);
-                 }
-             
-             break; 
-             
-             default:
-              break;  
-   
-         } // end switch
-           
-            
-       auto rc1 = signer_info_stmt->execute(); 
-       auto rc2 = signer_comments_stmt->execute(); 
-         
-       cout << "Result of signer_info_stmt->execute() = " << rc1 << endl;
-       cout << "Result of signer_comments_stmt->execute() = " << rc2 << endl;
-        
-   } catch (std::length_error &e) {
-       
-          cerr << "length_error exception caught: " << e.what() << '\n';
-          cerr << "Terminating" << "\n";
-          throw e;
-   
-   } catch (SQLException & e) { 
-              
-            cerr << "Error code = " << e.getErrorCode() << endl;
-            
-            cerr << "MySQL State message = " << e.getSQLState() << endl;
-            
-   } catch (exception & e) {
+                case 0:
+                {
+                  int signee_no = stoi(strings[0]);
+                  
+                  signer_info_stmt->setInt(1, signee_no);
+                  signer_comments_stmt->setInt(1, signee_no);
+                }
+                break;
+                    
+                case 1:    
+                { // DATE: YYY-MM-DD
+                    string date { strings[1].substr(6, 4) + "-" + strings[1].substr(0, 2) + "-" + strings[1].substr(3, 2) };
+                    
+                    signer_info_stmt->setDateTime(2, date);
+                }
+                break; 
+      
+                case 2:    
+                {   // First Name 
+                    
+                    if (strings[2].empty()) {
+                         // Per http://forums.mysql.com/read.php?167,419402,421088#msg-421088, the 2nd parameter cab be be 0.   
+	                 signer_info_stmt->setNull(3, 0); 
+      
+                    } else {
+      
+                        signer_info_stmt->setString(3, strings[2]);
+                    }
+                }
+                break; 
+      
+                case 3:    
+                {  // Last Name 
+                    
+                    if (strings[3].empty()) {
+      
+                       signer_info_stmt->setNull(4, 0);
+                        
+                    } else {
+      
+                       signer_info_stmt->setString(4, strings[3]);
+                    }
+                }
+                break; 
+      
+                case 4:    
+                {   // City 
+                    
+                    if (strings[4].empty()) {
+      
+                       signer_info_stmt->setNull(5, 0);
+                        
+                    } else {
+      
+                       signer_info_stmt->setString(5, strings[4]);
+                    }
+                }
+                break; 
+                 
+                case 5:    
                    
-                  // catch-all for C++11 exceptions 
-                  cerr << "C++11 exception caught: " << e.what() << '\n';
-                  cerr << "Terminating" << "\n";
-                  throw e;
-   }
-   
- } // end for         
-}  // end while    
+                    // Comments 
+                    if (strings[5].empty()) {
+      
+                       signer_comments_stmt->setNull(2, 0);
+                        
+                    } else {
+                
+                       signer_comments_stmt->setString(2, strings[5]);
+                    }
+                
+                break; 
+                
+                default:
+                 break;  
+      
+            } // end switch
+              
+               
+          auto rc1 = signer_info_stmt->execute(); 
+          auto rc2 = signer_comments_stmt->execute(); 
+            
+          cout << "Result of signer_info_stmt->execute() = " << rc1 << endl;
+          cout << "Result of signer_comments_stmt->execute() = " << rc2 << endl;
+           
+      } catch (std::length_error &e) {
+          
+             cerr << "length_error exception caught: " << e.what() << '\n';
+             cerr << "Terminating" << "\n";
+             throw e;
+      
+      } catch (SQLException & e) { 
+                 
+               cerr << "Error code = " << e.getErrorCode() << endl;
+               
+               cerr << "MySQL State message = " << e.getSQLState() << endl;
+               
+      } catch (exception & e) {
+                      
+                     // catch-all for C++11 exceptions 
+                     cerr << "C++11 exception caught: " << e.what() << '\n';
+                     cerr << "Terminating" << "\n";
+                     throw e;
+      } 
+     } // end for         
+   }  // end while    
        
   return(0);
 }
