7531fe49c112c8092bc2a7fc1d94abe422a90711
Kurt Krueckeberg <kurtk@pobox.com>
2015-04-16 14:50:21 -0500

    latest updates

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <cstdlib>
-#include <iostream>
-#include <memory>     // for unique_ptr, etc
+#include <iostream>   // debug only
+#include <memory>     // unique_ptr
 #include <exception>
 #include <vector> 
 #include <fstream>
 #include "csvparser.h"
+using namespace std;
 
 // MySQL Connector for C++
-#include <mysql_driver.h>  
+#include <mysql_driver.h>     // in static library 
 #include <mysql_connection.h> 
 
-#include <cppconn/driver.h>
+#include <cppconn/driver.h>   // in dynamic library
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 #include <cppconn/prepared_statement.h>
 
-using namespace std;
 using namespace sql; 
 
 /*
@@ -36,8 +36,6 @@ int main(int argc, char** argv)
 
  CsvParser csv_parser(argv[1]);
    
- // TODO: A transaction support later.
-
  // Credentials: (url, user, password)
    
  unique_ptr<Connection> conn { get_driver_instance()->connect("tcp://127.0.0.1:3306", "petition", "kk0457") };
@@ -53,11 +51,11 @@ int main(int argc, char** argv)
 
  unique_ptr<PreparedStatement> signer_comments_stmt { conn->prepareStatement("INSERT INTO signer_comments(signee_no, comments) VALUES(?, ?)") };
 
- unique_ptr<ResultSet> res { stmt->executeQuery("select max(signee_no) as max_signee FROM signer_info") };
+ unique_ptr<ResultSet> resultSet { stmt->executeQuery("select max(signee_no) as max_signee FROM signer_info") };
  
- res->first();
+ resultSet->first();
  
- auto max_signee = res->getUInt("max_signee"); 
+ auto max_signee = resultSet->getUInt("max_signee"); 
  
  while (csv_parser.hasmoreLines()) {  
 
@@ -65,7 +63,7 @@ int main(int argc, char** argv)
 
    int signee_no = stoi(strings[0]);
 
-   if (signee_no <= max_signee) { // check if it is already in DB.
+   if (signee_no <= max_signee) { // ignore if it is already in DB.
 
    	continue;
    }
@@ -77,6 +75,24 @@ int main(int argc, char** argv)
         cout << strings[i] << endl;     
         
         bool isEmpty { strings[i].empty() };
+
+        /*
+         * If not signee_no or date signed, then, if empty, invoke setNull()
+         */
+        if (i >= 2 && isEmpty) { 
+
+            // According to http://forums.mysql.com/read.php?167,419402,421088#msg-421088, the 2nd parameter can simply be be 0.   
+            if (i == 5) {
+
+                 signer_comments_stmt->setString(2,0); 
+ 
+            } else {
+
+                 signer_info_stmt->setNull(i + 1, 0); 
+            }
+
+            continue;
+        }
 
         switch(i) {
 
@@ -96,59 +112,30 @@ int main(int argc, char** argv)
            break; 
      
            case 2:    
-           {   // First Name 
+              // First Name 
                
-               if (isEmpty) {
-                   
-                    // According to http://forums.mysql.com/read.php?167,419402,421088#msg-421088, the 2nd parameter can simply be be 0.   
-                    signer_info_stmt->setNull(3, 0); 
-     
-               } else {
-     
-                   signer_info_stmt->setString(3, strings[2]);
-               }
-           }
+                signer_info_stmt->setString(3, strings[2]);
+           
            break; 
      
            case 3:    
-           {  // Last Name 
+             // Last Name 
                
-               if (isEmpty) {
-     
-                  signer_info_stmt->setNull(4, 0);
-                   
-               } else {
-     
-                  signer_info_stmt->setString(4, std::move(strings[3]));
-               }
-           }
+                signer_info_stmt->setString(4, std::move(strings[3]));
+
            break; 
      
            case 4:    
-           {   // City 
-               
-               if (isEmpty) {
-     
-                  signer_info_stmt->setNull(5, 0);
-                   
-               } else {
-     
-                  signer_info_stmt->setString(5, std::move(strings[4]));
-               }
-           }
+               // City 
+                
+                signer_info_stmt->setString(5, std::move(strings[4]));
+            
            break; 
             
            case 5:    
-              
                // Comments 
-               if (isEmpty) {
-     
-                  signer_comments_stmt->setNull(2, 0);
-                   
-               } else {
-           
-                  signer_comments_stmt->setString(2, std::move(strings[5]));
-               }
+                      
+               signer_comments_stmt->setString(2, std::move(strings[5]));
            
            break; 
            
