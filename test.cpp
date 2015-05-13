08011862a04505c5fe95b1af56165e81a150f5e3
Kurt Krueckeberg <kurtk@pobox.com>
2015-05-13 15:05:38 -0500

    more debug changes

--- test.cpp
+++ test.cpp
@@ -1,23 +1,35 @@
 #include <cstdlib>
-#include <memory>     // unique_ptr
-#include <exception>
-#include <vector> 
-#include <fstream>
-#include <iostream>               // debug
+//#include <utility>
+//#include <algorithm>
+#include <iterator>
 #include <regex>
-#include <utility>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 
-bool hasmoreLines(ifstream& input);
-string getNextLine(ifstream& input, const regex& csv_regex);
+void dumpLine(const string& line);
+string parseLine(std::ifstream& input, const std::regex& csv_regex);
+bool hasmoreLines(istream& input);
+
+void dumpLine(const string& line)
+{
+  for (auto iter = line.begin(); iter != line.end(); ++iter) {
+
+        int i = reinterpret_cast<char>(*iter);
+
+        cout << *iter << " as int: " << i << endl;
+  }
+ 
+}
 
 bool hasmoreLines(ifstream& input) 
 {
     char c;
     
     input >> c;
-
     bool bResult = input.good();
     
     if (bResult) {
@@ -28,61 +40,85 @@ bool hasmoreLines(ifstream& input)
     return bResult; 
 }
 
-string getNextLine(ifstream& input, const regex& csv_regex)
+string parseLine(ifstream& input, const regex& csv_regex)
 {
 smatch match;
-string prior_line, line;
-  
-auto fail_count = 0;
+string prior_line;
 
+int failed_count = 0;
+smatch short_match;
+ostringstream ostr;
+
+string line;
+   
  while (1) {
 
-   getline(input, line); // line is this->line
-   cout << "getline() = " << line << endl;
+   getline(input, line); 
    
    if (input.fail()) {
        
-       return string();
+       throw logic_error("no input to read");
    }
-   
+
+   if (failed_count && regex_search(line, short_match, regex {R"(^(\d+),(\d\d-\d\d-\d\d\d\d),)" })) {
+       
+        auto xx = ++short_match.begin();
+        
+        ostr << "\nRegex failed and next line is a new signer that starts with " << xx->str() << endl;
+
+        throw logic_error(ostr.str());
+   }
+
    // Replace any two consecutive double quotes with a single quote
-   line = prior_line + regex_replace(line, regex {"(\"\")"}, string{"'"}); // BUG: Failing, I believe that iconv removed the two double quotes?
+   line = prior_line + regex_replace(line, regex {"(\"\")"}, string{"'"}); 
    
-   //cout << line << endl; // debug only
+   cout << line << endl; 
 
    bool rc = regex_search(line, match, csv_regex);
  
    if (rc) { 
        
-       break;    
+       // Skip first hit, the entire regex. We only want the submatches. 
+       //copy(++(match.begin()), match.end(), emplace_inserter); 
+       
+      break;    
  
    } else {
-       ++fail_count;
- 
-       if (fail_count >= 9) {
-
-            throw logic_error("regex has failed 9 times ");
-       } 
-       cout << "regex_search() fail count is " << fail_count << " Re-attempting " << endl;
+       ++failed_count; 
        prior_line = std::move(line);
    }
+
  } 
 
- return line; 
+ return line;
 }
 
+bool hasmoreLines(istream& input) 
+{
+    char c;
+    
+    input >> c;
+    bool bResult = input.good();
+    
+    if (bResult) {
+        
+        input.putback(c);
+    }
+    
+    return bResult; 
+}
 
 int main(int argc, char** argv) 
-{
-    
+{    
   if (argc != 2) {
   
       cerr << "Please re-run with the input file name as the first parameter.\n";
       return 0;
   } 
-  
+
+  const string file_name(argv[1]);
+
   ifstream input;
-  string file_name {argv[1]};
 
   input.open(file_name);
 
@@ -91,32 +127,20 @@ int main(int argc, char** argv)
       throw logic_error("Could not open file" + file_name + "\n");
   }
 
-/*
- * Format of CSV file:
- * 
- *   Signer number,Date,"First Name","Last Name",City,State/Province,Country,"Why is this issue important to you?"
- */
+  const regex csv_regex{ R"(^(\d+),(\d\d-\d\d-\d\d\d\d),(?:"[^"]*"|[^,"]*),(?:"[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*)$)"};
   
-const regex csv_regex{ R"(^(\d+),(\d\d-\d\d-\d\d\d\d),(?:"[^"]*"|[^,"]*),(?:"[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*),("[^"]*"|[^,"]*)$)" };
-   
-int lineno = 1;
-
-while (hasmoreLines(input)) {  
-
   try {
+      
+  
+   while (hasmoreLines(input)) {
+    
+     string line = parseLine(input, csv_regex); 
 
-   string line = getNextLine(input, csv_regex); 
-   cout << "After getNextLine() = " << line << endl;
-
-    ++lineno;
-
-  } catch (exception& e) {
-
-    cerr << e.what() << endl;
-    return 0;
-
+     cout << line << '\n' << endl;
+   }
+  } catch (exception &e) {
+      
+      cerr << e.what() << endl;
   }
-}  // end while   
- 
   return(0);
 }
