21bd8a10872d040b465f94b93537d20823284cb7
Kurt Krueckeberg <kurtk@pobox.com>
2015-06-07 14:48:28 -0500

    code

--- fix-test.cpp
+++ fix-test.cpp
@@ -2,42 +2,25 @@
 #include <iostream>
 #include <regex>
 #include <exception>
-/*
-void fixTextTest(string& s)
-{
-    std::string s = "123 apples 456 oranges 789 bananas oranges bananas";
-    std::regex r("[a-z]+");
 
-    for(std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), r);
-                            i != std::sregex_iterator();
-                            ++i )
-    {
-        std::smatch m = *i;
-        std::cout << m.str() << " at position " << m.position() << '\n';
-
-      for(submatch& sub : m) {
-           cout << "submatch sub.str() " << sub.str() << " length of " << sub.length() << '\n';
-      }
-    }
-}
-*/
 using namespace std;
+
+/*
+ TODO: Calculate start of character to capitalize
+ */
 
 void fixText(string& s)
 {
    try {
       
-    std::regex r { R"((?:\.|\?|!)(\s+)[a-z])"};   
+    regex r { R"((?:\.|\?|!)(\s+)[a-z])"};   
     
     int total_offset = 0;
     
-    for(std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), r);
-                            i != std::sregex_iterator();
-                            ++i )
-    {
-        std::smatch m = *i;
-        //--std::cout << m.str() << " at position " << m.position() << " from total offset of " << total_offset << '\n';
-        std::cout << '"' << m.str() << '"' << " -- at offset " << total_offset + m.position() << " from total offset of " << total_offset << '\n';
+    for(auto iter =  sregex_iterator(s.begin(), s.end(), r);
+                        iter != sregex_iterator(); ++iter ) {
+        smatch m = *iter;
+        cout << '"' << m.str() << '"' << " -- at offset " << total_offset + m.position() << " from total offset of " << total_offset << '\n';
 
    
       for(auto& sub : m) {
