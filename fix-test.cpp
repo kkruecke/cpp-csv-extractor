d887990aaf6a128202849c1dc4e01edb780fc4ec
Kurt Krueckeberg <kurtk@pobox.com>
2015-06-07 18:09:25 -0500

    code

--- fix-test.cpp
+++ fix-test.cpp
@@ -2,44 +2,48 @@
 #include <iostream>
 #include <regex>
 #include <exception>
+#include <cctype>
+#include <clocale>
 
 using namespace std;
 
-/*
- TODO: Calculate start of character to capitalize
- */
-
 void fixText(string& s)
 {
+   setlocale(LC_ALL, "en_US.iso88591"); 
+     
    try {
-      
-    regex r { R"((?:\.|\?|!)(\s+)[a-z])"};   
-    
+          
     int total_offset = 0;
-    
-    for(auto iter =  sregex_iterator(s.begin(), s.end(), r);
-                        iter != sregex_iterator(); ++iter ) {
-        smatch m = *iter;
+    /*
+     * Look for '.' or '!' or '?" followed by whitespace, followed by lowercase a-z.
+     */
+    /*
+     * TODO: convert to 
+     * 
+      transform( sregex_iterator(s.begin(), s.end(), regex { R"((?:\.|\?|!)(\s+)[a-z])"},
+                 sregex_iterator(), [] (smatch &m) {
+      
+                                     int index = total_offset + m.position() + m.length() - 1;
+      
+                                     s[index] = toupper(s[index]);
 
-        cout << '"' << m.str() << '"' << " -- at offset " << total_offset + m.position() << " from total offset of " << total_offset << '\n';
+                                      total_offset += m.position() + m[0].length();
+     *                                });
+     */
+    for(auto iter =  sregex_iterator(s.begin(), s.end(), regex { R"((?:\.|\?|!)(\s+)[a-z])"} );
+                        iter != sregex_iterator(); ++iter ) {
+
+        smatch m = *iter; //
 
         int index = total_offset + m.position() + m.length() - 1;
-
-        cout << "'" << s.at(index) << "'" << " -- at offset " << index << " from total offset of " << total_offset << '\n';
-
-   
-      for(auto& sub : m) {
-          
-           cout << "submatch sub.str() " << '"' << sub.str() << '"' <<  " -- has length of " << sub.length() << '\n';
-      }
         
-      total_offset += m.position() + m[0].length();
-      
+        s[index] = toupper(s[index]);
+
+        total_offset += m.position() + m[0].length();
     }
     
   } catch (exception& e) {
    
       cout << "Exception in fix-text.cpp...." << e.what() << endl;
   }    
-  
 }
